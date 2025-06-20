8b37ddda763e0cbc0681fddb737652de24fa14a3
pranaya BR <pranayabaladhandapani@gmail.com>
2025-06-20 13:11:12 +0530

    BR first push

---
+++ firstscript.py
@@ -0,0 +1,11 @@
+
+# Sample Python code to print even numbers from 1 to 10
+
+def print_even_numbers():
+    print("Even numbers from 1 to 10:")
+    for i in range(1, 11):
+        if i % 2 == 0:
+            print(i)
+
+# Call the function
+print_even_numbers()
